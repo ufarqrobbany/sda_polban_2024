6bcd9bbf84b01cafbfadeda4dcafc96e871b173d
ufarqrobbany <faruqrobbany@gmail.com>
2024-02-29 19:29:45 +0700

    update ptr3 & ptr4

--- praktikum/pointer/ptr3.cpp
+++ praktikum/pointer/ptr3.cpp
@@ -4,12 +4,19 @@
 /* Tujuan : Menggunakan type data pointer dan manipulasinya */
 /* --------------------------------------------------------- */
 #include <stdio.h>
+
 int main() {
+    // Deklarasi variabel d bertipe float dan pointer pd bertipe float
     float d, *pd;
+    // Inisialisasi nilai variabel d
     d = 54.5;
+    // Mencetak nilai awal variabel d
     printf("Isi d semula = %g\n", d);
+    // Mengatur pointer pd untuk menunjuk ke alamat memori variabel d
     pd = &d;
+    // Mengubah nilai yang ditunjuk oleh pointer pd dengan menambahkannya 10
     *pd = *pd + 10;
+    // Mencetak nilai variabel d setelah diubah melalui pointer
     printf("Isi d kini = %g\n", d);
     return 0;
 }
