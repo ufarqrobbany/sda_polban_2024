ad347c3ae1a9b43e51d7ff04b9ce84d34123e88f
ufarqrobbany <faruqrobbany@gmail.com>
2024-03-01 09:17:31 +0700

    ptr7

---
+++ praktikum/pointer2/ptr7.cpp
@@ -0,0 +1,15 @@
+/* ----------------------------------------------------------- */
+/* File Program : PTR7.CPP */
+/* (Pointer dan String bagian ke-1) */
+/* Deskripsi : pointer yang menunjuk ke data string */
+/* ---------------------------------------------------------- */
+#include <stdio.h>
+int main() {
+    /* Kamus Data (deklarasi, inisialisasi nilai variabel)*/
+    char *pkota = "BANDUNG";
+    puts(pkota);
+    return 0;
+}
+/* STUDI KASUS : */
+/* Apa perbedaan deklarasi variabel char *pkota='BANDUNG'; dengan char kota[]='BANDUNG' ?
+ */
