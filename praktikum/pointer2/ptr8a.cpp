2ea0f29ec279c9626608db7521dd7914c90ee127
ufarqrobbany <faruqrobbany@gmail.com>
2024-03-01 09:18:09 +0700

    ptr8a

---
+++ praktikum/pointer2/ptr8a.cpp
@@ -0,0 +1,25 @@
+/* -------------------------------------------------------------------------------- */
+/* File Program : PTR8a.CPP */
+/* (Pointer dan String bagian ke-2) */
+/* Deskripsi : menukar isi dua buah string TANPA pemakaian pointer */
+/* ------------------------------------------------------------------------------- */
+#include <stdio.h>
+#include <string.h>
+#define PANJANG 20
+int main() {
+    /* Kamus Data (deklarasi, inisialisasi nilai variabel)*/
+    char nama1[PANJANG] = "DEWI SARTIKA";
+    char nama2[PANJANG] = "SULTAN HASANUDDIN";
+    char namaX[PANJANG];
+    puts("Nama semula : ");
+    printf("Nama 1 --> %s\n", nama1);
+    printf("Nama 2 --> %s\n", nama2);
+    /* Pertukaran string */
+    strcpy(namaX, nama1);
+    strcpy(nama1, nama2);
+    strcpy(nama2, namaX);
+    puts("Nama sekarang : ");
+    printf("Nama 1 --> %s\n", nama1);
+    printf("Nama 2 --> %s\n", nama2);
+    return 0;
+}
